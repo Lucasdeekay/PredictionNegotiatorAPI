39c509d7fa032d208f0c09b03ec910d7b4d6d686
Lucasdeekay <lucasdeekay98@gmail.com>
2024-03-28 21:41:44 +0100

    Edited settings.py

--- Negotiator/settings.py
+++ Negotiator/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'Negotiator.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_NAME'),
+        'NAME': os.getenv('POSTGRES_DATABASE'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST'),
