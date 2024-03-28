a7fb187a29ee0ad0084d6f29ac613fe86a690c6d
Lucasdeekay <lucasdeekay98@gmail.com>
2024-03-28 22:31:52 +0100

    Edited build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 # Install python dependencies
 python3 -m ensurepip --upgrade
-pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # Collect static files (adjust paths as needed)
-python manage.py collectstatic --no-input
+python3 manage.py collectstatic --no-input
