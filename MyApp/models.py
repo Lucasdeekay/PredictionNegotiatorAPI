ece81f471e3dcb62931b022313892957be73154f
Lucasdeekay <lucasdeekay98@gmail.com>
2024-03-31 00:38:50 +0100

    Edited project files

--- MyApp/models.py
+++ MyApp/models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 class Product(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     actual_price = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False)
+    discounted_price = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False)
     discount_percentage = models.IntegerField(null=False, blank=False)
     rating = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False)
     rating_count = models.IntegerField(null=False, blank=False)
